c84f19148aff81d481fffaadfd9eb7c74787cff8
zentel <fzt2009@163.com>
2021-09-08 18:01:09 +0800

    测试postTask

--- test/test.cpp
+++ test/test.cpp
@@ -10,16 +10,27 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*l
 int main(int argc,const char* argv) {
 #endif
 	CrashDump crash_dump;
-	CoInitialize(NULL);
+	if (CoInitialize(NULL) != 0) {
+		printf("CoInitialize error");
+		return -1;
+	}
 
 	DuiLib::CPaintManagerUI::SetResourcePath(_T("../skin/"));
 
 	duijs::JsEngine engine;
 	engine.Init();
+
 	bool rslt = engine.Excute("import {} from 'test.js'", "<eval>");
 	if (!rslt) {
 		return -1;
 	}
+
+
+	engine.PostTask([](qjs::Context* c) {
+		printf("run task\n");
+		});
+
+
 	engine.RunLoop();
 	CoUninitialize();
 	return 0;
