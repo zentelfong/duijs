ed7164f4711a3c11863d0e3fbd6b8f785d1fa05e
zentel <fzt2009@163.com>
2021-10-27 21:51:30 +0800

    实现weak_ptr

---
+++ utils/weak_ptr.h
@@ -0,0 +1,140 @@
+#pragma once
+#include <mutex>
+
+template<class T> class WeakPtr;
+
+template<class T>
+struct WeakImpl {
+	WeakImpl(T* p)
+		:ptr(p), ref(1)
+	{
+	}
+
+	bool valied() {
+		return ptr != nullptr;
+	}
+
+	void Freed() {
+		lock.lock();
+		ptr = nullptr;
+		int r = --ref;
+		lock.unlock();
+		if (r == 0) {
+			delete this;
+		}
+	}
+
+	int AddRef() {
+		std::lock_guard<std::recursive_mutex> guard(lock);
+		return ++ref;
+	}
+
+	int Release() {
+		lock.lock();
+		int r = --ref;
+		lock.unlock();
+		if (r == 0) {
+			delete this;
+		}
+		return r;
+	}
+
+	void Lock() {
+		lock.lock();
+	}
+
+	void Unlock() {
+		lock.unlock();
+	}
+
+	T* ptr;
+	int ref;
+	std::recursive_mutex lock;
+};
+
+
+
+template<class T>
+class WeakObject {
+public:
+	WeakObject()
+		:weak_impl_(new WeakImpl<T>(static_cast<T*>(this)))
+	{
+	}
+
+	~WeakObject() {
+		weak_impl_->Freed();
+	}
+
+	WeakPtr<T> weak_ptr() {
+		return WeakPtr<T>(weak_impl_);
+	}
+
+private:
+	friend class WeakPtr<T>;
+	WeakImpl<T>* weak_impl_;
+};
+
+
+
+template<class T>
+class WeakPtr {
+public:
+	WeakPtr()
+		:locked_(false), ptr_(nullptr)
+	{
+	}
+
+	WeakPtr(WeakImpl<T>* data)
+		:locked_(false), ptr_(data)
+	{
+		if(ptr_)
+			ptr_->AddRef();
+	}
+
+
+	WeakPtr(const WeakPtr<T>& r) :locked_(false), ptr_(r.ptr_)
+	{
+		if (ptr_)
+			ptr_->AddRef();
+	}
+
+	~WeakPtr() {
+		Unlock();
+		ptr_->Release();
+	}
+
+	WeakPtr<T>& operator=(WeakImpl<T>* p)
+	{
+		if (p) p->AddRef();
+		if (ptr_) ptr_->Release();
+		ptr_ = p;
+		return *this;
+	}
+
+	WeakPtr<T>& operator=(const WeakPtr<T>& r)
+	{
+		return *this = r.ptr_;
+	}
+
+	T* Lock() {
+		if (!locked_ && ptr_) {
+			locked_ = true;
+			ptr_->Lock();
+			return ptr_->ptr;
+		}
+		return nullptr;
+	}
+
+
+	void Unlock() {
+		if (locked_ && ptr_) {
+			locked_ = false;
+			ptr_->Unlock();
+		}
+	}
+private:
+	bool locked_;
+	WeakImpl<T>* ptr_;
+};
+
